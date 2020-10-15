a3952380fb20d4310b66e5b174b47fe4d321b7f7
RedKroker <31494194+RedKroker@users.noreply.github.com>
2020-10-14 21:05:40 -0400

    Update main.py

--- main.py
+++ main.py
@@ -3,10 +3,12 @@
 import sklearn
 import numpy as np
 from sklearn import datasets, svm, metrics
+from sklearn.metrics import confusion_matrix, precision_score, recall_score, f1_score, accuracy_score
 
 from sklearn.naive_bayes import GaussianNB
 from sklearn.model_selection import train_test_split
-import matplotlib.pyplot as plt
+from sklearn.datasets import load_digits
+from sklearn.linear_model import Perceptron
 import csv
 
 #
@@ -61,18 +63,62 @@ with open('test_with_label_1.csv') as csvfile:
     #     print(row)
     # print("List of outcomes: ", letters2)
 
-    npFeatures = np.array(features)
-    npFeatures = npFeatures.astype(np.float64)
-    npLetters = np.array(letters)
-    npLetters = npLetters.astype(np.float64)
+    trainFeatures = np.array(features)
+    trainFeatures = trainFeatures.astype(np.float64)
+    trainLabels = np.array(letters)
+    trainLabels = trainLabels.astype(np.float64)
 
-    npFeatures2 = np.array(features2)
-    npFeatures2 = npFeatures2.astype(np.float64)
+    testFeatures = np.array(features2)
+    testFeatures = testFeatures.astype(np.float64)
+    testLabels = np.array(letters2)
+    testLabels = testLabels.astype(np.float64)
 
-    clf = GaussianNB()
-    clf.fit(npFeatures, npLetters)
-    y_pred = clf.predict(npFeatures2)
-    print(y_pred)
+    # Perceptron
+    clf = Perceptron(tol=1e-3, random_state=0)
+    clf.fit(trainFeatures, trainLabels)
+    prediction = clf.predict(testFeatures)
+
+    precision = precision_score(testLabels, prediction, average =None)
+    recall = recall_score(testLabels, prediction, average =None)
+    f1 = f1_score(testLabels, prediction, average =None)
+    accuracy = accuracy_score(testLabels, prediction)
+    f1_macro = f1_score(testLabels, prediction, average ='macro')
+    f1_weighted = f1_score(testLabels, prediction, average ='weighted')
+
+# -----------------OUTPUT-----------------------
+
+# Confusion matrix
+np.savetxt("PER-DS1.csv", confusion_matrix(testLabels, prediction), delimiter=",", fmt='%d')
+print(testLabels)
+
+with open('PER-DS1.csv', mode='r+', newline='') as output_file:
+    output_writer = csv.writer(output_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+    content = output_file.read()
+    # output_file.seek(0,0) (To put pred at beginning of file, but it deletes some part of confusion matrix, to check)
+
+    # Predictions
+    output_writer.writerow("")
+    output_writer.writerow(["Predictions"])
+    for x in range(1, 81):
+        output_writer.writerow((x, int(prediction[x-1])))
+
+    # Precision, recall and f1
+    output_writer.writerow("")
+    output_writer.writerow(["Class,Precision,Recall,F1-measure"])
+    for x in range(1, 27):
+        output_writer.writerow((x, np.around(precision[x - 1], 2), np.around(recall[x - 1],2 ), np.around(f1[x - 1], 2)))
+
+    # Accuracy, macro-average f1 and weighted-average f1
+    output_writer.writerow("")
+    output_writer.writerow(("Accuracy", np.around(accuracy, 3)))
+    output_writer.writerow("")
+    output_writer.writerow(("Macro-average F1-measure", np.around(f1_macro, 3)))
+    output_writer.writerow("")
+    output_writer.writerow(("Weighted-average F1-measure", np.around(f1_weighted, 3)))
+
+
+
+
 
 
 
